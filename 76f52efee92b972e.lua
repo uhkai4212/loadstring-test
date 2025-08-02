82afc07dac9f27d0c42bea7ec77245252e9b6c0b
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:00 +1000

    Add 76f52efee92b972e.lua

---
+++ 76f52efee92b972e.lua
@@ -0,0 +1,7 @@
+DELTA["10da"] = Instance.new("ImageButton", DELTA["109"]);
+DELTA["10da"]["BackgroundColor3"] = Color3.fromRGB(59, 64, 79);
+DELTA["10da"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["10da"]["Image"] = [[rbxassetid://0]];
+DELTA["10da"]["Size"] = UDim2.new(0.09953451156616211, 0, 0.473985493183136, 0);
+DELTA["10da"]["Name"] = [[Button1]];
+DELTA["10da"]["Position"] = UDim2.new(0.9, 0, 0.5000000596046448, 0);
