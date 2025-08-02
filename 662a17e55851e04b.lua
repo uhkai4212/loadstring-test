a0f6fa6102fd9d6618d1cb9799ea819a18f393f0
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:47 +1000

    Add 662a17e55851e04b.lua

---
+++ 662a17e55851e04b.lua
@@ -0,0 +1,8 @@
+DELTA["28"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["28"]["AutoButtonColor"] = false;
+DELTA["28"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["28"]["LayoutOrder"] = 3;
+DELTA["28"]["Image"] = [[rbxassetid://0]];
+DELTA["28"]["Size"] = UDim2.new(0.5135507583618164, 0, 0.08553311228752136, 0);
+DELTA["28"]["Name"] = [[Scripthub]];
+DELTA["28"]["Position"] = UDim2.new(0.274707168340683, 0, 0.5514350533485413, 0);
