fd6d465cbf7cc499d5978fba9728426a047879ec
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:38 +1000

    Add 0497a379f35e8565.lua

---
+++ 0497a379f35e8565.lua
@@ -0,0 +1,7 @@
+DELTA["143"] = Instance.new("ImageButton", DELTA["13f"]);
+DELTA["143"]["BackgroundColor3"] = Color3.fromRGB(59, 64, 79);
+DELTA["143"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["143"]["Image"] = [[rbxassetid://0]];
+DELTA["143"]["Size"] = UDim2.new(0.09953451156616211, 0, 0.473985493183136, 0);
+DELTA["143"]["Name"] = [[Button]];
+DELTA["143"]["Position"] = UDim2.new(1.0157949924468994, 0, 0.5000000596046448, 0);
