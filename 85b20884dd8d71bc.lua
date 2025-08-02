a4ac081542d1769dfc5ebf3b32738058147f57c9
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:59 +1000

    Add 85b20884dd8d71bc.lua

---
+++ 85b20884dd8d71bc.lua
@@ -0,0 +1,7 @@
+DELTA["10d"] = Instance.new("ImageButton", DELTA["109"]);
+DELTA["10d"]["BackgroundColor3"] = Color3.fromRGB(59, 64, 79);
+DELTA["10d"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["10d"]["Image"] = [[rbxassetid://0]];
+DELTA["10d"]["Size"] = UDim2.new(0.09953451156616211, 0, 0.473985493183136, 0);
+DELTA["10d"]["Name"] = [[Button]];
+DELTA["10d"]["Position"] = UDim2.new(1.0157949924468994, 0, 0.5000000596046448, 0);
