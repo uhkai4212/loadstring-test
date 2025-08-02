1911dd26720feb71d8475582b1bd48dd2058effe
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:02 +1000

    Add 5edd6fbf49d0eb01.lua

---
+++ 5edd6fbf49d0eb01.lua
@@ -0,0 +1,57 @@
+DELTA["112"] = Instance.new("TextLabel", DELTA["110"]);
+DELTA["112"]["TextWrapped"] = true;
+DELTA["112"]["TextScaled"] = true;
+DELTA["112"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["112"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["112"]["TextSize"] = 14;
+DELTA["112"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["112"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["112"]["Size"] = UDim2.new(0.8315319418907166, 0, 0.5, 0);
+DELTA["112"]["Text"] = [[UTILITY]];
+DELTA["112"]["Name"] = [[Title]];
+DELTA["112"]["BackgroundTransparency"] = 1;
+DELTA["112"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Code
+DELTA["113"] = Instance.new("ScrollingFrame", DELTA["88"]);
+DELTA["113"]["ZIndex"] = 200;
+DELTA["113"]["BorderSizePixel"] = 0;
+DELTA["113"]["CanvasSize"] = UDim2.new(0, 0, 0, 0);
+DELTA["113"]["TopImage"] = [[rbxasset://textures/ui/Scroll/scroll-middle.png]];
+DELTA["113"]["BackgroundColor3"] = Color3.fromRGB(22, 22, 30);
+DELTA["113"]["HorizontalScrollBarInset"] = Enum.ScrollBarInset.Always;
+DELTA["113"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["113"]["AutomaticCanvasSize"] = Enum.AutomaticSize.XY;
+DELTA["113"]["BackgroundTransparency"] = 1;
+DELTA["113"]["Size"] = UDim2.new(0.9764046669006348, 0, 0.6893500685691833, 0);
+DELTA["113"]["Selectable"] = false;
+DELTA["113"].ElasticBehavior = Enum.ElasticBehavior.Never
+DELTA["113"]["ScrollBarThickness"] = 15;
+DELTA["113"]["Position"] = UDim2.new(0.5117977261543274, 0, 0.8393499851226807, 0);
+DELTA["113"]["Name"] = [[Code]];
+DELTA["113"]["BottomImage"] = [[rbxasset://textures/ui/Scroll/scroll-middle.png]];
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Frame
+DELTA["114"] = Instance.new("Frame", DELTA["88"]);
+DELTA["114"]["ZIndex"] = 201;
+DELTA["114"]["BorderSizePixel"] = 0;
+DELTA["114"]["BackgroundColor3"] = Color3.fromRGB(34, 41, 50);
+DELTA["114"]["Size"] = UDim2.new(1.0011208057403564, 0, 0.006693750154227018, 0);
+DELTA["114"]["Position"] = UDim2.new(5.143397530105176e-08, 0, 0.14056874811649323, 0);
+
+-- StarterGui.Delta.Executor.Executor.UICorner
+DELTA["115"] = Instance.new("UICorner", DELTA["87"]);
+DELTA["115"]["CornerRadius"] = UDim.new(0.05000000074505806, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu
+DELTA["116"] = Instance.new("ImageLabel", DELTA["86"]);
+DELTA["116"]["BorderSizePixel"] = 0;
+DELTA["116"]["ScaleType"] = Enum.ScaleType.Crop;
+DELTA["116"]["BackgroundColor3"] = Color3.fromRGB(36, 0, 0);
+DELTA["116"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["116"]["Image"] = [[rbxassetid://0]];
+DELTA["116"]["Size"] = UDim2.new(0.3273969292640686, 0, 0.9993020296096802, 0);
+DELTA["116"]["ClipsDescendants"] = true;
+DELTA["116"]["Name"] = [[Sidemenu]];
+DELTA["116"]["BackgroundTransparency"] = 1;
+DELTA["116"]["Position"] = UDim2.new(-0.004258748609572649, 0, 0.5005565881729126, 0);
