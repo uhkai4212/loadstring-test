dfe8444372234c2f314c856428838692a1603683
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:40 +1000

    Add abc0e80ed9500aff.lua

---
+++ abc0e80ed9500aff.lua
@@ -0,0 +1,7 @@
+DELTA["3e"] = Instance.new("ImageLabel", DELTA["3c"]);
+DELTA["3e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["3e"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["3e"]["Image"] = [[rbxassetid://13365156882]];
+DELTA["3e"]["Size"] = UDim2.new(0.025552265346050262, 0, 0.40771156549453735, 0);
+DELTA["3e"]["BackgroundTransparency"] = 1;
+DELTA["3e"]["Position"] = UDim2.new(0.01834862306714058, 0, 0.5, 0);
