d90db7ac9cd4419848260cf372cad19b57176d3f
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:15 +1000

    Add 1b74720a003ff97c.lua

---
+++ 1b74720a003ff97c.lua
@@ -0,0 +1,7 @@
+DELTA["11a"] = Instance.new("Frame", DELTA["119"]);
+DELTA["11a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["11a"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["11a"]["BackgroundTransparency"] = 1;
+DELTA["11a"]["Size"] = UDim2.new(0.8012143969535828, 0, 0.7959624528884888, 0);
+DELTA["11a"]["Position"] = UDim2.new(0.5, 0, 0.49274569749832153, 0);
+DELTA["11a"]["Name"] = [[Holder]];
