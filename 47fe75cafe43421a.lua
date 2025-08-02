60a8a679263a7657b40bc21b17cf20678afe045f
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:07 +1000

    Add 47fe75cafe43421a.lua

---
+++ 47fe75cafe43421a.lua
@@ -0,0 +1,8 @@
+DELTA["6b"] = Instance.new("ImageButton", DELTA["52"]);
+DELTA["6b"]["ZIndex"] = 999999999;
+DELTA["6b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["6b"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["6b"]["Image"] = [[rbxassetid://13363121645]];
+DELTA["6b"]["Size"] = UDim2.new(0.04585733264684677, 0, 0.04716602712869644, 0);
+DELTA["6b"]["Position"] = UDim2.new(0.8981863260269165, 0, 0.15145258605480194, 0);
+DELTA["6b"]["BackgroundTransparency"] = 1;
