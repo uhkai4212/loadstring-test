4b4285f918c4d882da2fa249b844d5cef32f5696
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:15 +1000

    Add cea48b763b59fc1f.lua

---
+++ cea48b763b59fc1f.lua
@@ -0,0 +1,6 @@
+DELTA["11b"] = Instance.new("Frame", DELTA["11a"]);
+DELTA["11b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["11b"]["BackgroundTransparency"] = 1;
+DELTA["11b"]["Size"] = UDim2.new(1.0907094478607178, 0, 0.3451063930988312, 0);
+DELTA["11b"]["Position"] = UDim2.new(-0.045354731380939484, 0, 0.041554853320121765, 0);
+DELTA["11b"]["Name"] = [[Title]];
