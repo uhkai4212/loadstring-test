ae163b1ac240640a0c8fd04fb9507c4f2a4655ee
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:27 +1000

    Add 529084d11a498061.lua

---
+++ 529084d11a498061.lua
@@ -0,0 +1,8 @@
+DELTA["109"] = Instance.new("ImageLabel", DELTA["bf"]);
+DELTA["109"]["BackgroundColor3"] = Color3.fromRGB(38, 41, 50);
+DELTA["109"]["LayoutOrder"] = 1;
+DELTA["109"]["Size"] = UDim2.new(0.997948408126831, 0, 0.0462365560233593, 0);
+DELTA["109"]["Position"] = UDim2.new(0, 0, 1.0919346493665216e-07, 0);
+DELTA["109"]["Name"] = [[Script]];
+DELTA["109"].Active = true;
+DELTA["109"].Visible = false
