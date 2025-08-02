4a2f0b8acd33bc086e87d98dbd3f7760cff02375
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:57 +1000

    Add 062ec8f431e02b3a.lua

---
+++ 062ec8f431e02b3a.lua
@@ -0,0 +1,226 @@
+DELTA["a2"] = Instance.new("LocalScript", DELTA["8a"]);
+
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons
+DELTA["a3"] = Instance.new("Frame", DELTA["88"]);
+DELTA["a3"]["ZIndex"] = 200;
+DELTA["a3"]["BorderSizePixel"] = 0;
+DELTA["a3"]["BackgroundColor3"] = Color3.fromRGB(22, 22, 30);
+DELTA["a3"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["a3"]["BackgroundTransparency"] = 1;
+DELTA["a3"]["Size"] = UDim2.new(0.96828693151474, 0, 0.17342007160186768, 0);
+DELTA["a3"]["ClipsDescendants"] = true;
+DELTA["a3"]["Position"] = UDim2.new(0.507856547832489, 0, 0.9821560382843018, 0);
+DELTA["a3"]["Name"] = [[Buttons]];
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.UIListLayout
+DELTA["a4"] = Instance.new("UIListLayout", DELTA["a3"]);
+DELTA["a4"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["a4"]["FillDirection"] = Enum.FillDirection.Horizontal;
+DELTA["a4"]["Padding"] = UDim.new(0.029999999329447746, 0);
+DELTA["a4"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Execute
+DELTA["a5"] = Instance.new("ImageButton", DELTA["a3"]);
+DELTA["a5"]["Active"] = false;
+DELTA["a5"]["ZIndex"] = 300;
+DELTA["a5"]["BackgroundColor3"] = Color3.fromRGB(60, 137, 196);
+DELTA["a5"]["Selectable"] = false;
+DELTA["a5"]["Size"] = UDim2.new(0.2561585009098053, 0, 0.7000001668930054, 0);
+DELTA["a5"]["Name"] = [[Execute]];
+DELTA["a5"]["Position"] = UDim2.new(0.008000015281140804, 0, 0.20599937438964844, 0);
+DELTA["a5"]["BackgroundTransparency"] = 0.8899999856948853;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Execute.UICorner
+DELTA["a6"] = Instance.new("UICorner", DELTA["a5"]);
+DELTA["a6"]["CornerRadius"] = UDim.new(0.30000001192092896, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Execute.Title
+DELTA["a7"] = Instance.new("TextLabel", DELTA["a5"]);
+DELTA["a7"]["TextWrapped"] = true;
+DELTA["a7"]["TextScaled"] = true;
+DELTA["a7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["a7"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["a7"]["TextSize"] = 14;
+DELTA["a7"]["TextColor3"] = Color3.fromRGB(140, 206, 255);
+DELTA["a7"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["a7"]["Size"] = UDim2.new(0.8766257762908936, 0, 0.3993089199066162, 0);
+DELTA["a7"]["Text"] = [[EXECUTE]];
+DELTA["a7"]["Name"] = [[Title]];
+DELTA["a7"]["BackgroundTransparency"] = 1;
+DELTA["a7"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Execute.UIStroke
+DELTA["a8"] = Instance.new("UIStroke", DELTA["a5"]);
+DELTA["a8"]["Color"] = Color3.fromRGB(60, 137, 196);
+DELTA["a8"]["Thickness"] = 2;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Clear
+DELTA["a9"] = Instance.new("ImageButton", DELTA["a3"]);
+DELTA["a9"]["Active"] = false;
+DELTA["a9"]["ZIndex"] = 300;
+DELTA["a9"]["BackgroundColor3"] = Color3.fromRGB(94, 136, 169);
+DELTA["a9"]["Selectable"] = false;
+DELTA["a9"]["LayoutOrder"] = 1;
+DELTA["a9"]["Size"] = UDim2.new(0.22138313949108124, 0, 0.7000001668930054, 0);
+DELTA["a9"]["Name"] = [[Clear]];
+DELTA["a9"]["Position"] = UDim2.new(0.27973994612693787, 0, 0.20599937438964844, 0);
+DELTA["a9"]["BackgroundTransparency"] = 0.9300000071525574;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Clear.UICorner
+DELTA["aa"] = Instance.new("UICorner", DELTA["a9"]);
+DELTA["aa"]["CornerRadius"] = UDim.new(0.30000001192092896, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Clear.Title
+DELTA["ab"] = Instance.new("TextLabel", DELTA["a9"]);
+DELTA["ab"]["TextWrapped"] = true;
+DELTA["ab"]["TextScaled"] = true;
+DELTA["ab"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["ab"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["ab"]["TextSize"] = 14;
+DELTA["ab"]["TextColor3"] = Color3.fromRGB(185, 211, 230);
+DELTA["ab"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["ab"]["Size"] = UDim2.new(0.8766260147094727, 0, 0.3993089199066162, 0);
+DELTA["ab"]["Text"] = [[CLEAR]];
+DELTA["ab"]["Name"] = [[Title]];
+DELTA["ab"]["BackgroundTransparency"] = 1;
+DELTA["ab"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Clear.UIStroke
+DELTA["ac"] = Instance.new("UIStroke", DELTA["a9"]);
+DELTA["ac"]["Color"] = Color3.fromRGB(69, 97, 119);
+DELTA["ac"]["Thickness"] = 2;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.ExecuteClipboard
+DELTA["ad"] = Instance.new("ImageButton", DELTA["a3"]);
+DELTA["ad"]["Active"] = false;
+DELTA["ad"]["ZIndex"] = 300;
+DELTA["ad"]["BackgroundColor3"] = Color3.fromRGB(94, 136, 169);
+DELTA["ad"]["Selectable"] = false;
+DELTA["ad"]["LayoutOrder"] = 2;
+DELTA["ad"]["Size"] = UDim2.new(0.3552889823913574, 0, 0.699999988079071, 0);
+DELTA["ad"]["Name"] = [[ExecuteClipboard]];
+DELTA["ad"]["Position"] = UDim2.new(0.5057284832000732, 0, 0.20600003004074097, 0);
+DELTA["ad"]["BackgroundTransparency"] = 0.9300000071525574;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.ExecuteClipboard.UICorner
+DELTA["ae"] = Instance.new("UICorner", DELTA["ad"]);
+DELTA["ae"]["CornerRadius"] = UDim.new(0.30000001192092896, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.ExecuteClipboard.Title
+DELTA["af"] = Instance.new("TextLabel", DELTA["ad"]);
+DELTA["af"]["TextWrapped"] = true;
+DELTA["af"]["TextScaled"] = true;
+DELTA["af"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["af"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["af"]["TextSize"] = 14;
+DELTA["af"]["TextColor3"] = Color3.fromRGB(185, 211, 230);
+DELTA["af"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["af"]["Size"] = UDim2.new(0.8766259551048279, 0, 0.3993089497089386, 0);
+DELTA["af"]["Text"] = [[EXECUTE CLIPBOARD]];
+DELTA["af"]["Name"] = [[Title]];
+DELTA["af"]["BackgroundTransparency"] = 1;
+DELTA["af"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.ExecuteClipboard.UIStroke
+DELTA["b0"] = Instance.new("UIStroke", DELTA["ad"]);
+DELTA["b0"]["Color"] = Color3.fromRGB(69, 97, 119);
+DELTA["b0"]["Thickness"] = 2;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.UIPadding
+DELTA["b1"] = Instance.new("UIPadding", DELTA["a3"]);
+DELTA["b1"]["PaddingTop"] = UDim.new(0.07999999821186066, 0);
+DELTA["b1"]["PaddingRight"] = UDim.new(0.00800000037997961, 0);
+DELTA["b1"]["PaddingBottom"] = UDim.new(0.07999999821186066, 0);
+DELTA["b1"]["PaddingLeft"] = UDim.new(0.00800000037997961, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.ButtonHandlers
+DELTA["b2"] = Instance.new("LocalScript", DELTA["a3"]);
+DELTA["b2"]["Name"] = [[ButtonHandlers]];
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Button3
+DELTA["b3"] = Instance.new("TextBox", DELTA["a3"]);
+DELTA["b3"]["Active"] = true;
+DELTA["b3"]["ZIndex"] = 300;
+DELTA["b3"]["TextSize"] = 21;
+DELTA["b3"]["TextWrapped"] = true;
+DELTA["b3"]["BackgroundColor3"] = Color3.fromRGB(94, 136, 169);
+DELTA["b3"]["TextColor3"] = Color3.fromRGB(185, 211, 230);
+DELTA["b3"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["b3"]["LayoutOrder"] = 2;
+DELTA["b3"]["BackgroundTransparency"] = 0.9300000071525574;
+DELTA["b3"]["Size"] = UDim2.new(0.07424724847078323, 0, 0.699999988079071, 0);
+DELTA["b3"]["Selectable"] = false;
+DELTA["b3"]["Text"] = [[]];
+DELTA["b3"]["Position"] = UDim2.new(0.9160652756690979, 0, 0.20599988102912903, 0);
+DELTA["b3"]["Visible"] = false;
+DELTA["b3"]["Name"] = [[Button3]];
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Button3.UICorner
+DELTA["b4"] = Instance.new("UICorner", DELTA["b3"]);
+DELTA["b4"]["CornerRadius"] = UDim.new(0.30000001192092896, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Buttons.Button3.UIStroke
+DELTA["b5"] = Instance.new("UIStroke", DELTA["b3"]);
+DELTA["b5"]["Color"] = Color3.fromRGB(69, 97, 119);
+DELTA["b5"]["Thickness"] = 2;
+DELTA["b5"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs
+DELTA["b6"] = Instance.new("ScrollingFrame", DELTA["88"]);
+DELTA["b6"]["ZIndex"] = 200;
+DELTA["b6"]["BorderSizePixel"] = 0;
+DELTA["b6"]["CanvasSize"] = UDim2.new(2, 0, 0, 0);
+DELTA["b6"]["BackgroundColor3"] = Color3.fromRGB(22, 22, 30);
+DELTA["b6"]["HorizontalScrollBarInset"] = Enum.ScrollBarInset.Always;
+DELTA["b6"]["AutomaticCanvasSize"] = Enum.AutomaticSize.X;
+DELTA["b6"]["Size"] = UDim2.new(0.9840000867843628, 0, 0.12545627355575562, 0);
+DELTA["b6"]["Selectable"] = false;
+DELTA["b6"]["ScrollBarThickness"] = 0;
+DELTA["b6"]["Position"] = UDim2.new(0.007965609431266785, 0, 0.016980938613414764, 0);
+DELTA["b6"]["Name"] = [[Tabs]];
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.UIPadding
+DELTA["b7"] = Instance.new("UIPadding", DELTA["b6"]);
+DELTA["b7"]["PaddingTop"] = UDim.new(0.07999999821186066, 0);
+DELTA["b7"]["PaddingRight"] = UDim.new(0.00800000037997961, 0);
+DELTA["b7"]["PaddingBottom"] = UDim.new(0.07999999821186066, 0);
+DELTA["b7"]["PaddingLeft"] = UDim.new(0.00800000037997961, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.UIListLayout
+DELTA["b8"] = Instance.new("UIListLayout", DELTA["b6"]);
+DELTA["b8"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["b8"]["FillDirection"] = Enum.FillDirection.Horizontal;
+DELTA["b8"]["Padding"] = UDim.new(0.009999999776482582, 0);
+DELTA["b8"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.AddTab
+DELTA["b9"] = Instance.new("ImageButton", DELTA["b6"]);
+DELTA["b9"]["Active"] = false;
+DELTA["b9"]["ZIndex"] = 300;
+DELTA["b9"]["BackgroundColor3"] = Color3.fromRGB(34, 41, 50);
+DELTA["b9"]["Selectable"] = false;
+DELTA["b9"]["LayoutOrder"] = 1000;
+DELTA["b9"]["Size"] = UDim2.new(0.0526634119451046, 0, 0.6154626607894897, 0);
+DELTA["b9"]["Name"] = [[AddTab]];
+DELTA["b9"]["Position"] = UDim2.new(0.26442891359329224, 0, 0.30375349521636963, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.AddTab.UICorner
+DELTA["ba"] = Instance.new("UICorner", DELTA["b9"]);
+DELTA["ba"]["CornerRadius"] = UDim.new(0.30000001192092896, 0);
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.AddTab.ImageButton
+DELTA["bb"] = Instance.new("ImageButton", DELTA["b9"]);
+DELTA["bb"]["Active"] = false;
+DELTA["bb"]["Modal"] = true;
+DELTA["bb"]["AutoButtonColor"] = false;
+DELTA["bb"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["bb"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["bb"]["Image"] = [[rbxassetid://13387627918]];
+DELTA["bb"]["Size"] = UDim2.new(0.35981613397598267, 0, 0.3613884449005127, 0);
+DELTA["bb"]["Rotation"] = 45;
+DELTA["bb"]["Position"] = UDim2.new(0.699999988079071, 0, 0.5, 0);
+DELTA["bb"]["BackgroundTransparency"] = 1;
+
+-- StarterGui.Delta.Executor.Executor.Overlay.Tabs.AddTab.ImageButton.UIAspectRatioConstraint
+DELTA["bc"] = Instance.new("UIAspectRatioConstraint", DELTA["bb"]);
