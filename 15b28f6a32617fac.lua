079a980c4f9d6aa5f1d1a1bf2ca3d06745f4b8e7
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:59 +1000

    Add 15b28f6a32617fac.lua

---
+++ 15b28f6a32617fac.lua
@@ -0,0 +1,5 @@
+DELTA["58"] = Instance.new("UIListLayout", DELTA["57"]);
+DELTA["58"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["58"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+DELTA["58"]["Padding"] = UDim.new(0.05000000074505806, 0);
+DELTA["58"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
