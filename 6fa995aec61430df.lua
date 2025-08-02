331c9125f87b026c9f9ce543afc3cff1eea2b9d6
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:39 +1000

    Add 6fa995aec61430df.lua

---
+++ 6fa995aec61430df.lua
@@ -0,0 +1,5 @@
+DELTA["3c"] = Instance.new("Frame", DELTA["3b"]);
+DELTA["3c"]["ZIndex"] = 2;
+DELTA["3c"]["BackgroundColor3"] = Color3.fromRGB(38, 41, 50);
+DELTA["3c"]["Size"] = UDim2.new(1, 0, 0.12585513293743134, 0);
+DELTA["3c"]["Name"] = [[Searchbar]];
