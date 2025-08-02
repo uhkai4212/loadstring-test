84f5a626adc5b53b25e24c05d065ef3cd9cd5593
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:29 +1000

    Add 8dc685c4eaf0b898.lua

---
+++ 8dc685c4eaf0b898.lua
@@ -0,0 +1,14 @@
+DELTA["10f"] = Instance.new("TextLabel", DELTA["10d"]);
+DELTA["10f"]["TextWrapped"] = true;
+DELTA["10f"]["ZIndex"] = 999999999;
+DELTA["10f"]["TextScaled"] = true;
+DELTA["10f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["10f"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["10f"]["TextSize"] = 14;
+DELTA["10f"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["10f"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["10f"]["Size"] = UDim2.new(0.7953082323074341, 0, 0.44124072790145874, 0);
+DELTA["10f"]["Text"] = [[EXECUTE]];
+DELTA["10f"]["Name"] = [[Title]];
+DELTA["10f"]["BackgroundTransparency"] = 1;
+DELTA["10f"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
