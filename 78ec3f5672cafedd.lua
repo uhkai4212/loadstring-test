cbde551e46eaf1af60f985bd2910c5483be6696f
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:16 +1000

    Add 78ec3f5672cafedd.lua

---
+++ 78ec3f5672cafedd.lua
@@ -0,0 +1,8 @@
+DELTA["11d"] = Instance.new("TextButton", DELTA["11a"]);
+DELTA["11d"]["BackgroundColor3"] = Color3.fromRGB(32, 35, 46);
+DELTA["11d"]["Selectable"] = false;
+DELTA["11d"]["Size"] = UDim2.new(1.0907094478607178, 0, 0.48145753145217896, 0);
+DELTA["11d"]["Name"] = [[Showcase]];
+DELTA["11d"]["Text"] = [[]];
+DELTA["11d"]["Position"] = UDim2.new(-0.0453546978533268, 0, 0.41410815715789795, 0);
+DELTA["11d"]["BackgroundTransparency"] = 0.4000000059604645;
