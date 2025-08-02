102cbaf6dc5d2519d367cc330165c355279cd8b9
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:18 +1000

    Add 63c00dc803d71976.lua

---
+++ 63c00dc803d71976.lua
@@ -0,0 +1,46 @@
+DELTA["122"] = Instance.new("LocalScript", DELTA["117"]);
+DELTA["122"]["Name"] = [[ScriptSuggestionHandler]];
+
+-- StarterGui.Delta.Executor.Sidemenu.Network
+DELTA["123"] = Instance.new("ImageLabel", DELTA["116"]);
+DELTA["123"]["ZIndex"] = 99999;
+DELTA["123"]["BackgroundColor3"] = Color3.fromRGB(26, 27, 36);
+DELTA["123"]["Image"] = [[rbxassetid://0]];
+DELTA["123"]["Size"] = UDim2.new(1.0000001192092896, 0, 0.3936704099178314, 0);
+DELTA["123"]["ClipsDescendants"] = true;
+DELTA["123"]["Name"] = [[Network]];
+DELTA["123"]["Position"] = UDim2.new(-1.3669654208570137e-07, 0, 0.6052098870277405, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.UICorner
+DELTA["124"] = Instance.new("UICorner", DELTA["123"]);
+DELTA["124"]["CornerRadius"] = UDim.new(0.05000000074505806, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay
+DELTA["125"] = Instance.new("Frame", DELTA["123"]);
+DELTA["125"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["125"]["BackgroundTransparency"] = 1;
+DELTA["125"]["Size"] = UDim2.new(1, 0, 1, 0);
+DELTA["125"]["Name"] = [[Overlay]];
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder
+DELTA["126"] = Instance.new("Frame", DELTA["125"]);
+DELTA["126"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["126"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["126"]["BackgroundTransparency"] = 1;
+DELTA["126"]["Size"] = UDim2.new(0.8999999761581421, 0, 0.8310460448265076, 0);
+DELTA["126"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+DELTA["126"]["Name"] = [[Holder]];
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.UIListLayout
+DELTA["127"] = Instance.new("UIListLayout", DELTA["126"]);
+DELTA["127"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["127"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+DELTA["127"]["Padding"] = UDim.new(0.15000000596046448, 0);
+DELTA["127"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Frame
+DELTA["128"] = Instance.new("Frame", DELTA["126"]);
+DELTA["128"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["128"]["BackgroundTransparency"] = 1;
+DELTA["128"]["Size"] = UDim2.new(1, 0, 0.40413346886634827, 0);
+DELTA["128"]["Position"] = UDim2.new(0, 0, 0.027302570641040802, 0);
