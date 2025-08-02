4fea9470b177dc1c6b2aefccfbee096dd730a5a0
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:06 +1000

    Add 48cf3312d7f47fbc.lua

---
+++ 48cf3312d7f47fbc.lua
@@ -0,0 +1,4 @@
+DELTA["11f"] = Instance.new("UIListLayout", DELTA["11d"]);
+DELTA["11f"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["11f"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+DELTA["11f"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
