636691a50d35c77bd8b9c3454fdba3242f1f3fe3
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:10 +1000

    Add 86faa9b15c6f5a79.lua

---
+++ 86faa9b15c6f5a79.lua
@@ -0,0 +1,5 @@
+DELTA["146"] = Instance.new("Frame", DELTA["13f"]);
+DELTA["146"]["BackgroundColor3"] = Color3.fromRGB(86, 173, 239);
+DELTA["146"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["146"]["Size"] = UDim2.new(0.08429200947284698, 0, 0.3846021890640259, 0);
+DELTA["146"]["Position"] = UDim2.new(0.14588697254657745, 0, 0.49755123257637024, 0);
