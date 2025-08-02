76139297b6bc201964855c77d58c1e8e3ccfbdeb
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:33 +1000

    Add 23553357bc67cd3f.lua

---
+++ 23553357bc67cd3f.lua
@@ -0,0 +1,5 @@
+DELTA["119"] = Instance.new("Frame", DELTA["117"]);
+DELTA["119"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["119"]["BackgroundTransparency"] = 1;
+DELTA["119"]["Size"] = UDim2.new(1, 0, 1, 0);
+DELTA["119"]["Name"] = [[Overlay]];
