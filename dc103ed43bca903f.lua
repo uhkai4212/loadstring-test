449a42d914bee1ee1264eefc8ae8f36f389df1d3
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:53 +1000

    Add dc103ed43bca903f.lua

---
+++ dc103ed43bca903f.lua
@@ -0,0 +1,7 @@
+DELTA["48"] = Instance.new("Frame", DELTA["46"]);
+DELTA["48"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["48"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["48"]["BackgroundTransparency"] = 1;
+DELTA["48"]["Size"] = UDim2.new(0.8982645273208618, 0, 0.5242727994918823, 0);
+DELTA["48"]["Position"] = UDim2.new(0.04966278374195099, 0, 0.3592950105667114, 0);
+DELTA["48"]["Name"] = [[Title]];
