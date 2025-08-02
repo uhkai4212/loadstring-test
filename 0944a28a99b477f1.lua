01760869f0825bfb9fae3d43eee2c22c12e9721c
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:07 +1000

    Add 0944a28a99b477f1.lua

---
+++ 0944a28a99b477f1.lua
@@ -0,0 +1,219 @@
+DELTA["129"] = Instance.new("TextLabel", DELTA["128"]);
+DELTA["129"]["TextWrapped"] = true;
+DELTA["129"]["TextScaled"] = true;
+DELTA["129"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["129"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+DELTA["129"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Medium, Enum.FontStyle.Normal);
+DELTA["129"]["TextSize"] = 14;
+DELTA["129"]["TextColor3"] = Color3.fromRGB(157, 162, 199);
+DELTA["129"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["129"]["Size"] = UDim2.new(0.9877889156341553, 0, 0.33333709836006165, 0);
+DELTA["129"]["Text"] = [[Client status information]];
+DELTA["129"]["Name"] = [[Description]];
+DELTA["129"]["BackgroundTransparency"] = 1;
+DELTA["129"]["Position"] = UDim2.new(0.5, 0, 0.9999999403953552, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Frame.Title
+DELTA["12a"] = Instance.new("TextLabel", DELTA["128"]);
+DELTA["12a"]["TextWrapped"] = true;
+DELTA["12a"]["TextScaled"] = true;
+DELTA["12a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["12a"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+DELTA["12a"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["12a"]["TextSize"] = 14;
+DELTA["12a"]["TextColor3"] = Color3.fromRGB(190, 206, 232);
+DELTA["12a"]["AnchorPoint"] = Vector2.new(0.5, 0);
+DELTA["12a"]["Size"] = UDim2.new(0.9877890348434448, 0, 0.482876718044281, 0);
+DELTA["12a"]["Text"] = [[Network]];
+DELTA["12a"]["Name"] = [[Title]];
+DELTA["12a"]["BackgroundTransparency"] = 1;
+DELTA["12a"]["Position"] = UDim2.new(0.5, 0, 0, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information
+DELTA["12b"] = Instance.new("Frame", DELTA["126"]);
+DELTA["12b"]["BackgroundColor3"] = Color3.fromRGB(32, 35, 46);
+DELTA["12b"]["BackgroundTransparency"] = 0.4000000059604645;
+DELTA["12b"]["Size"] = UDim2.new(1, 0, 0.3164331018924713, 0);
+DELTA["12b"]["Position"] = UDim2.new(0, 0, 0.6628726124763489, 0);
+DELTA["12b"]["Name"] = [[Information]];
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information.UICorner
+DELTA["12c"] = Instance.new("UICorner", DELTA["12b"]);
+DELTA["12c"]["CornerRadius"] = UDim.new(1, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information.UIListLayout
+DELTA["12d"] = Instance.new("UIListLayout", DELTA["12b"]);
+DELTA["12d"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["12d"]["FillDirection"] = Enum.FillDirection.Horizontal;
+DELTA["12d"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+DELTA["12d"]["Padding"] = UDim.new(0.029999999329447746, 0);
+DELTA["12d"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information.Memory
+DELTA["12e"] = Instance.new("TextLabel", DELTA["12b"]);
+DELTA["12e"]["TextWrapped"] = true;
+DELTA["12e"]["RichText"] = true;
+DELTA["12e"]["TextScaled"] = true;
+DELTA["12e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["12e"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.SemiBold, Enum.FontStyle.Normal);
+DELTA["12e"]["TextSize"] = 14;
+DELTA["12e"]["TextColor3"] = Color3.fromRGB(157, 162, 199);
+DELTA["12e"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["12e"]["Size"] = UDim2.new(0.28310254216194153, 0, 0.2934793531894684, 0);
+DELTA["12e"]["Text"] = [[<font color="#4FA4F2">68</font> MB]];
+DELTA["12e"]["Name"] = [[Memory]];
+DELTA["12e"]["BackgroundTransparency"] = 1;
+DELTA["12e"]["Position"] = UDim2.new(0.05447632819414139, 0, 0.6467397809028625, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information.Ping
+DELTA["12f"] = Instance.new("TextLabel", DELTA["12b"]);
+DELTA["12f"]["TextWrapped"] = true;
+DELTA["12f"]["RichText"] = true;
+DELTA["12f"]["TextScaled"] = true;
+DELTA["12f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["12f"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.SemiBold, Enum.FontStyle.Normal);
+DELTA["12f"]["TextSize"] = 14;
+DELTA["12f"]["TextColor3"] = Color3.fromRGB(157, 162, 199);
+DELTA["12f"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["12f"]["LayoutOrder"] = 2;
+DELTA["12f"]["Size"] = UDim2.new(0.3066037893295288, 0, 0.2934792935848236, 0);
+DELTA["12f"]["Text"] = [[<font color="#4FA4F2">68</font> MS Ping]];
+DELTA["12f"]["Name"] = [[Ping]];
+DELTA["12f"]["BackgroundTransparency"] = 1;
+DELTA["12f"]["Position"] = UDim2.new(0.2835494577884674, 0, 0.646739661693573, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.Overlay.Holder.Information.Players
+DELTA["130"] = Instance.new("TextLabel", DELTA["12b"]);
+DELTA["130"]["TextWrapped"] = true;
+DELTA["130"]["RichText"] = true;
+DELTA["130"]["TextScaled"] = true;
+DELTA["130"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["130"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.SemiBold, Enum.FontStyle.Normal);
+DELTA["130"]["TextSize"] = 14;
+DELTA["130"]["TextColor3"] = Color3.fromRGB(157, 162, 199);
+DELTA["130"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["130"]["LayoutOrder"] = 3;
+DELTA["130"]["Size"] = UDim2.new(0.3066039979457855, 0, 0.2934792935848236, 0);
+DELTA["130"]["Text"] = [[<font color="#4FA4F2">11</font> players]];
+DELTA["130"]["Name"] = [[Players]];
+DELTA["130"]["BackgroundTransparency"] = 1;
+DELTA["130"]["Position"] = UDim2.new(0.6138899326324463, 0, 0.646739661693573, 0);
+
+-- StarterGui.Delta.Executor.Sidemenu.Network.NetworkStatsHandler
+DELTA["131"] = Instance.new("LocalScript", DELTA["123"]);
+DELTA["131"]["Name"] = [[NetworkStatsHandler]];
+
+-- StarterGui.Delta.Executor.Sidemenu.UIListLayout
+DELTA["132"] = Instance.new("UIListLayout", DELTA["116"]);
+DELTA["132"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["132"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Right;
+DELTA["132"]["Padding"] = UDim.new(0.05000000074505806, 0);
+DELTA["132"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Executor.Marker
+DELTA["133"] = Instance.new("StringValue", DELTA["86"]);
+DELTA["133"]["Value"] = [[Menu]];
+DELTA["133"]["Name"] = [[Marker]];
+
+-- StarterGui.Delta.Home
+DELTA["134"] = Instance.new("Frame", DELTA["1"]);
+DELTA["134"]["ZIndex"] = 100;
+DELTA["134"]["BackgroundColor3"] = Color3.fromRGB(255, 0, 0);
+DELTA["134"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["134"]["BackgroundTransparency"] = 1;
+DELTA["134"]["Size"] = UDim2.new(0.8483448028564453, 0, 0.8924814462661743, 0);
+DELTA["134"]["Position"] = UDim2.new(0.47624671459198, 0, 0.5082324147224426, 0);
+DELTA["134"]["Visible"] = false;
+DELTA["134"]["Name"] = [[Home]];
+
+-- StarterGui.Delta.Home.Searchbar
+DELTA["135"] = Instance.new("Frame", DELTA["134"]);
+DELTA["135"]["ZIndex"] = 2;
+DELTA["135"]["BackgroundColor3"] = Color3.fromRGB(38, 41, 50);
+DELTA["135"]["Size"] = UDim2.new(1, 0, 0.12585513293743134, 0);
+DELTA["135"]["Name"] = [[Searchbar]];
+
+-- StarterGui.Delta.Home.Searchbar.UICorner
+DELTA["136"] = Instance.new("UICorner", DELTA["135"]);
+DELTA["136"]["CornerRadius"] = UDim.new(0.20000000298023224, 0);
+
+-- StarterGui.Delta.Home.Searchbar.ImageLabel
+DELTA["137"] = Instance.new("ImageLabel", DELTA["135"]);
+DELTA["137"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["137"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["137"]["Image"] = [[rbxassetid://13365156882]];
+DELTA["137"]["Size"] = UDim2.new(0.025552265346050262, 0, 0.40771156549453735, 0);
+DELTA["137"]["BackgroundTransparency"] = 1;
+DELTA["137"]["Position"] = UDim2.new(0.01834862306714058, 0, 0.5, 0);
+
+-- StarterGui.Delta.Home.Searchbar.ImageLabel.UIAspectRatioConstraint
+DELTA["138"] = Instance.new("UIAspectRatioConstraint", DELTA["137"]);
+
+
+-- StarterGui.Delta.Home.Searchbar.Input
+DELTA["139"] = Instance.new("TextBox", DELTA["135"]);
+DELTA["139"]["Active"] = true;
+DELTA["139"]["TextSize"] = 14;
+DELTA["139"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+DELTA["139"]["TextWrapped"] = true;
+DELTA["139"]["TextScaled"] = true;
+DELTA["139"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["139"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["139"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+DELTA["139"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["139"]["BackgroundTransparency"] = 1;
+DELTA["139"]["PlaceholderText"] = [[Search for scripts...]];
+DELTA["139"]["Size"] = UDim2.new(0.8766564130783081, 0, 0.35361653566360474, 0);
+DELTA["139"]["Selectable"] = false;
+DELTA["139"]["Text"] = [[]];
+DELTA["139"]["Position"] = UDim2.new(0.061162080615758896, 0, 0.49999991059303284, 0);
+DELTA["139"]["Name"] = [[Input]];
+
+-- StarterGui.Delta.Home.Searchbar.Button
+DELTA["13a"] = Instance.new("ImageButton", DELTA["135"]);
+DELTA["13a"]["ZIndex"] = 999999999;
+DELTA["13a"]["BackgroundColor3"] = Color3.fromRGB(59, 139, 254);
+DELTA["13a"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["13a"]["Image"] = [[rbxassetid://0]];
+DELTA["13a"]["Size"] = UDim2.new(0.08857108652591705, 0, 0.572917103767395, 0);
+DELTA["13a"]["Name"] = [[Button]];
+DELTA["13a"]["Position"] = UDim2.new(0.982576847076416, 0, 0.4999999701976776, 0);
+
+-- StarterGui.Delta.Home.Searchbar.Button.UICorner
+DELTA["13b"] = Instance.new("UICorner", DELTA["13a"]);
+DELTA["13b"]["CornerRadius"] = UDim.new(0.15000000596046448, 0);
+
+-- StarterGui.Delta.Home.Searchbar.Button.Title
+DELTA["13c"] = Instance.new("TextLabel", DELTA["13a"]);
+DELTA["13c"]["TextWrapped"] = true;
+DELTA["13c"]["ZIndex"] = 999999999;
+DELTA["13c"]["TextScaled"] = true;
+DELTA["13c"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["13c"]["FontFace"] = Font.new([[rbxassetid://11702779517]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+DELTA["13c"]["TextSize"] = 14;
+DELTA["13c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["13c"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["13c"]["Size"] = UDim2.new(0.7953082323074341, 0, 0.44124072790145874, 0);
+DELTA["13c"]["Text"] = [[UPLOAD]];
+DELTA["13c"]["Name"] = [[Title]];
+DELTA["13c"]["BackgroundTransparency"] = 1;
+DELTA["13c"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Home.Holder
+DELTA["13d"] = Instance.new("ScrollingFrame", DELTA["134"]);
+DELTA["13d"]["CanvasSize"] = UDim2.new(0, 0, 3, 0);
+DELTA["13d"]["ScrollBarImageTransparency"] = 1;
+DELTA["13d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["13d"]["VerticalScrollBarInset"] = Enum.ScrollBarInset.Always;
+DELTA["13d"]["AnchorPoint"] = Vector2.new(0.5, 1);
+DELTA["13d"]["BackgroundTransparency"] = 1;
+DELTA["13d"]["Size"] = UDim2.new(1, 0, 0.8350700736045837, 0);
+DELTA["13d"]["Selectable"] = false;
+DELTA["13d"]["ScrollBarThickness"] = 0;
+DELTA["13d"]["Position"] = UDim2.new(0.5, 0, 1, 0);
+DELTA["13d"]["Name"] = [[Holder]];
+
+-- StarterGui.Delta.Home.Holder.UIListLayout
+DELTA["13e"] = Instance.new("UIListLayout", DELTA["13d"]);
+DELTA["13e"]["Padding"] = UDim.new(0.004999999888241291, 0);
+DELTA["13e"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
