498f2dd5e12b9a57f3195a082c496d3e3b276982
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:20 +1000

    Add 0a6901354cd09759.lua

---
+++ 0a6901354cd09759.lua
@@ -0,0 +1,3 @@
+DELTA["142"] = Instance.new("UIPadding", DELTA["13f"]);
+DELTA["142"]["PaddingRight"] = UDim.new(0.029999999329447746, 0);
+DELTA["142"]["PaddingLeft"] = UDim.new(0.029999999329447746, 0);
