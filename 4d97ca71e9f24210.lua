0db25c865e6adaa2b664a95fac30c464943c1fd2
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:57:14 +1000

    Add 4d97ca71e9f24210.lua

---
+++ 4d97ca71e9f24210.lua
@@ -0,0 +1,7 @@
+DELTA["117"] = Instance.new("ImageLabel", DELTA["116"]);
+DELTA["117"]["BackgroundColor3"] = Color3.fromRGB(26, 27, 36);
+DELTA["117"]["Image"] = [[rbxassetid://0]];
+DELTA["117"]["Size"] = UDim2.new(1.0000001192092896, 0, 0.4340279698371887, 0);
+DELTA["117"]["ClipsDescendants"] = true;
+DELTA["117"]["Name"] = [[Script]];
+DELTA["117"]["Position"] = UDim2.new(-1.131473865711996e-07, 0, 0.09011479467153549, 0);
