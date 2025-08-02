9e90540e6cab5261a862c49400e938bb0d153105
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:46 +1000

    Add 942777ded07e0048.lua

---
+++ 942777ded07e0048.lua
@@ -0,0 +1,7 @@
+DELTA["4e"] = Instance.new("Frame", DELTA["46"]);
+DELTA["4e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["4e"]["AnchorPoint"] = Vector2.new(0, 0.5);
+DELTA["4e"]["BackgroundTransparency"] = 1;
+DELTA["4e"]["Size"] = UDim2.new(0.8982645869255066, 0, 0.233791321516037, 0);
+DELTA["4e"]["Position"] = UDim2.new(0.04966278374195099, 0, 0.7775326371192932, 0);
+DELTA["4e"]["Name"] = [[Views]];
