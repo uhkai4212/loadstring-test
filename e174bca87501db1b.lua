e758248abba8772726460cafe1b815e0cb650698
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:55:43 +1000

    Add e174bca87501db1b.lua

---
+++ e174bca87501db1b.lua
@@ -0,0 +1,5 @@
+DELTA["46"] = Instance.new("Frame", DELTA["44"]);
+DELTA["46"]["BackgroundColor3"] = Color3.fromRGB(26, 27, 36);
+DELTA["46"]["BackgroundTransparency"] = 0.09200000017881393;
+DELTA["46"]["Size"] = UDim2.new(1, 0, 1, 0);
+DELTA["46"]["Name"] = [[Overlay]];
