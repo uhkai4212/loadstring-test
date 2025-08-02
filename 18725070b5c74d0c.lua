45ddafa50a82f4cb11ec085917f0c0127522cf83
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:08 +1000

    Add 18725070b5c74d0c.lua

---
+++ 18725070b5c74d0c.lua
@@ -0,0 +1,136 @@
+DELTA["2b"] = Instance.new("ImageLabel", DELTA["28"]);
+DELTA["2b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["2b"]["ImageColor3"] = Color3.fromRGB(138, 145, 164);
+DELTA["2b"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["2b"]["Image"] = [[rbxassetid://13462270380]];
+DELTA["2b"]["Size"] = UDim2.new(0.40817680954933167, 0, 0.3085578680038452, 0);
+DELTA["2b"]["BackgroundTransparency"] = 1;
+DELTA["2b"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Sidebar.Settings
+DELTA["2c"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["2c"]["AutoButtonColor"] = false;
+DELTA["2c"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["2c"]["LayoutOrder"] = 4;
+DELTA["2c"]["Image"] = [[rbxassetid://0]];
+DELTA["2c"]["Size"] = UDim2.new(0.5135504603385925, 0, 0.08553305268287659, 0);
+DELTA["2c"]["Name"] = [[Settings]];
+DELTA["2c"]["Position"] = UDim2.new(0.274707168340683, 0, 0.6815073490142822, 0);
+
+-- StarterGui.Delta.Sidebar.Settings.UICorner
+DELTA["2d"] = Instance.new("UICorner", DELTA["2c"]);
+DELTA["2d"]["CornerRadius"] = UDim.new(0.4000000059604645, 0);
+
+-- StarterGui.Delta.Sidebar.Settings.UIAspectRatioConstraint
+DELTA["2e"] = Instance.new("UIAspectRatioConstraint", DELTA["2c"]);
+
+
+-- StarterGui.Delta.Sidebar.Settings.ImageLabel
+DELTA["2f"] = Instance.new("ImageLabel", DELTA["2c"]);
+DELTA["2f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["2f"]["ImageColor3"] = Color3.fromRGB(138, 145, 164);
+DELTA["2f"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["2f"]["Image"] = [[rbxassetid://13462271348]];
+DELTA["2f"]["Size"] = UDim2.new(0.4556732177734375, 0, 0.4556732177734375, 0);
+DELTA["2f"]["BackgroundTransparency"] = 1;
+DELTA["2f"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Sidebar.Executor
+DELTA["30"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["30"]["AutoButtonColor"] = false;
+DELTA["30"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["30"]["LayoutOrder"] = 2;
+DELTA["30"]["Image"] = [[rbxassetid://0]];
+DELTA["30"]["Size"] = UDim2.new(0.5135505795478821, 0, 0.08553306758403778, 0);
+DELTA["30"]["Name"] = [[Executor]];
+DELTA["30"]["Position"] = UDim2.new(0.274707168340683, 0, 0.42136284708976746, 0);
+
+-- StarterGui.Delta.Sidebar.Executor.UIAspectRatioConstraint
+DELTA["31"] = Instance.new("UIAspectRatioConstraint", DELTA["30"]);
+
+
+-- StarterGui.Delta.Sidebar.Executor.UICorner
+DELTA["32"] = Instance.new("UICorner", DELTA["30"]);
+DELTA["32"]["CornerRadius"] = UDim.new(0.4000000059604645, 0);
+
+-- StarterGui.Delta.Sidebar.Executor.ImageLabel
+DELTA["33"] = Instance.new("ImageLabel", DELTA["30"]);
+DELTA["33"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["33"]["ImageColor3"] = Color3.fromRGB(138, 145, 164);
+DELTA["33"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["33"]["Image"] = [[rbxassetid://13462269327]];
+DELTA["33"]["Size"] = UDim2.new(0.4556732177734375, 0, 0.4556732177734375, 0);
+DELTA["33"]["BackgroundTransparency"] = 1;
+DELTA["33"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+-- StarterGui.Delta.Sidebar.Console
+DELTA["34"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["34"]["AutoButtonColor"] = false;
+DELTA["34"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["34"]["LayoutOrder"] = 4;
+DELTA["34"]["Size"] = UDim2.new(0.5135504603385925, 0, 0.08553305268287659, 0);
+DELTA["34"]["Name"] = [[Console]];
+DELTA["34"]["Position"] = UDim2.new(0.274707168340683, 0, 0.6815073490142822, 0);
+
+-- StarterGui.Delta.Sidebar.Console.UICorner
+DELTA["35"] = Instance.new("UICorner", DELTA["34"]);
+DELTA["35"]["CornerRadius"] = UDim.new(0.4000000059604645, 0);
+
+-- StarterGui.Delta.Sidebar.Console.UIAspectRatioConstraint
+DELTA["36"] = Instance.new("UIAspectRatioConstraint", DELTA["34"]);
+
+
+-- StarterGui.Delta.Sidebar.Console.ImageLabel
+DELTA["37"] = Instance.new("ImageLabel", DELTA["34"]);
+DELTA["37"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["37"]["ImageColor3"] = Color3.fromRGB(137, 139, 172);
+DELTA["37"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["37"]["Image"] = [[rbxassetid://13569242972]];
+DELTA["37"]["Size"] = UDim2.new(0.4556732177734375, 0, 0.4556732177734375, 0);
+DELTA["37"]["BackgroundTransparency"] = 1;
+DELTA["37"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+
+
+-- StarterGui.Delta.Sidebar.ToggleUI
+DELTA["34tf"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["34tf"]["AutoButtonColor"] = false;
+DELTA["34tf"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["34tf"]["LayoutOrder"] = 4;
+DELTA["34tf"]["Size"] = UDim2.new(0.5135504603385925, 0, 0.08553305268287659, 0);
+DELTA["34tf"]["Name"] = [[ToggleUI]];
+DELTA["34tf"]["Position"] = UDim2.new(0.274707168340683, 0, 0.6815073490142822, 0);
+
+-- StarterGui.Delta.Sidebar.ToggleUI.UICorner
+DELTA["35tf"] = Instance.new("UICorner", DELTA["34tf"]);
+DELTA["35tf"]["CornerRadius"] = UDim.new(0.4000000059604645, 0);
+
+-- StarterGui.Delta.Sidebar.ToggleUI.UIAspectRatioConstraint
+DELTA["36tf"] = Instance.new("UIAspectRatioConstraint", DELTA["34tf"]);
+
+
+-- StarterGui.Delta.Sidebar.ToggleUI.ImageLabel
+DELTA["37tf"] = Instance.new("ImageLabel", DELTA["34tf"]);
+DELTA["37tf"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["37tf"]["ImageColor3"] = Color3.fromRGB(255,255,255);
+DELTA["37tf"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["37tf"]["Image"] = [[http://www.roblox.com/asset?id=13711943220]];
+DELTA["37tf"]["Size"] = UDim2.new(0.4556732177734375, 0, 0.4556732177734375, 0);
+DELTA["37tf"]["BackgroundTransparency"] = 1;
+DELTA["37tf"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+
+
+-- StarterGui.Delta.Sidebar.ActiveColor
+DELTA["38"] = Instance.new("Color3Value", DELTA["22"]);
+DELTA["38"]["Value"] = Color3.fromRGB(34, 160, 255);
+DELTA["38"]["Name"] = [[ActiveColor]];
+
+-- StarterGui.Delta.Sidebar.SidebarHandler
+DELTA["39"] = Instance.new("LocalScript", DELTA["22"]);
+DELTA["39"]["Name"] = [[SidebarHandler]];
+
+-- StarterGui.Delta.Sidebar.InactiveColor
+DELTA["3a"] = Instance.new("Color3Value", DELTA["22"]);
+DELTA["3a"]["Value"] = Color3.fromRGB(41, 45, 58);
+DELTA["3a"]["Name"] = [[InactiveColor]];
