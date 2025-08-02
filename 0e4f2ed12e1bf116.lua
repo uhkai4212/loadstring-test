d004392071aa45c7a31404c12ab0f8639d9e4577
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:07 +1000

    Add 0e4f2ed12e1bf116.lua

---
+++ 0e4f2ed12e1bf116.lua
@@ -0,0 +1,47 @@
+DELTA["21"] = Instance.new("LocalScript", DELTA["2"]);
+
+
+-- StarterGui.Delta.Sidebar
+DELTA["22"] = Instance.new("Frame", DELTA["1"]);
+DELTA["22"]["BorderSizePixel"] = 0;
+DELTA["22"]["BackgroundColor3"] = Color3.fromRGB(24, 25, 33);
+DELTA["22"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["22"]["Size"] = UDim2.new(0.07823482155799866, 0, 1.17552649974823, 0);
+DELTA["22"]["Position"] = UDim2.new(0.9997662901878357, 0, 0.4742202162742615, 0);
+DELTA["22"]["Visible"] = false;
+DELTA["22"]["Name"] = [[Sidebar]];
+
+-- StarterGui.Delta.Sidebar.UIListLayout
+DELTA["23"] = Instance.new("UIListLayout", DELTA["22"]);
+DELTA["23"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+DELTA["23"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+DELTA["23"]["Padding"] = UDim.new(0.03999999910593033, 0);
+DELTA["23"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.Delta.Sidebar.Home
+DELTA["24"] = Instance.new("ImageButton", DELTA["22"]);
+DELTA["24"]["AutoButtonColor"] = false;
+DELTA["24"]["BackgroundColor3"] = Color3.fromRGB(41, 45, 58);
+DELTA["24"]["LayoutOrder"] = 1;
+DELTA["24"]["Image"] = [[rbxassetid://0]];
+DELTA["24"]["Size"] = UDim2.new(0.5135505795478821, 0, 0.08553311228752136, 0);
+DELTA["24"]["Name"] = [[Home]];
+DELTA["24"]["Position"] = UDim2.new(0.274707168340683, 0, 0.29129067063331604, 0);
+
+-- StarterGui.Delta.Sidebar.Home.UIAspectRatioConstraint
+DELTA["25"] = Instance.new("UIAspectRatioConstraint", DELTA["24"]);
+
+
+-- StarterGui.Delta.Sidebar.Home.UICorner
+DELTA["26"] = Instance.new("UICorner", DELTA["24"]);
+DELTA["26"]["CornerRadius"] = UDim.new(0.4000000059604645, 0);
+
+-- StarterGui.Delta.Sidebar.Home.ImageLabel
+DELTA["27"] = Instance.new("ImageLabel", DELTA["24"]);
+DELTA["27"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["27"]["ImageColor3"] = Color3.fromRGB(138, 145, 164);
+DELTA["27"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+DELTA["27"]["Image"] = [[rbxassetid://13462268450]];
+DELTA["27"]["Size"] = UDim2.new(0.4556732177734375, 0, 0.4556732177734375, 0);
+DELTA["27"]["BackgroundTransparency"] = 1;
+DELTA["27"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
