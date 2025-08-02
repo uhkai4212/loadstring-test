3a076d60f7a0edf4a727969484191eaaaab2cbd6
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:07 +1000

    Add 3a141d277fc82948.lua

---
+++ 3a141d277fc82948.lua
@@ -0,0 +1,6 @@
+DELTA["13f"] = Instance.new("Frame", DELTA["13d"]);
+DELTA["13f"]["BackgroundColor3"] = Color3.fromRGB(38, 41, 50);
+DELTA["13f"]["LayoutOrder"] = 1;
+DELTA["13f"]["Size"] = UDim2.new(0.997948408126831, 0, 0.0462365560233593, 0);
+DELTA["13f"]["Position"] = UDim2.new(0, 0, 1.0919346493665216e-07, 0);
+DELTA["13f"]["Name"] = [[Script]];
