59af9588f3c7f94543fe5f78ea988bafe44f79b4
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:56:55 +1000

    Add 73997afb80d54503.lua

---
+++ 73997afb80d54503.lua
@@ -0,0 +1,6 @@
+DELTA["4b"] = Instance.new("Frame", DELTA["48"]);
+DELTA["4b"]["BackgroundColor3"] = Color3.fromRGB(86, 173, 239);
+DELTA["4b"]["AnchorPoint"] = Vector2.new(1, 0.5);
+DELTA["4b"]["Size"] = UDim2.new(0.3242603540420532, 0, 0.3846021890640259, 0);
+DELTA["4b"]["Position"] = UDim2.new(0.9999999403953552, 0, 0.2522916793823242, 0);
+DELTA["4b"]["Name"] = [[Verified]];
