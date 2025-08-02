af80abc78c9493dc5a31f3e9ea37dd44bc0c9100
uhkai4212 <kyeburke1@gmail.com>
2025-08-02 10:54:17 +1000

    Add 8e6eda4761ef991f.lua

---
+++ 8e6eda4761ef991f.lua
@@ -0,0 +1,8 @@
+DELTA["53"] = Instance.new("Frame", DELTA["52"]);
+DELTA["53"]["ZIndex"] = 999999999;
+DELTA["53"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+DELTA["53"]["AnchorPoint"] = Vector2.new(0.5, 0);
+DELTA["53"]["BackgroundTransparency"] = 1;
+DELTA["53"]["Size"] = UDim2.new(0.8193565607070923, 0, 0.23179079592227936, 0);
+DELTA["53"]["Position"] = UDim2.new(0.5, 0, 0.09408924728631973, 0);
+DELTA["53"]["Name"] = [[Title]];
