5a27553f85a95d883fda3e465e346bbf07ac3301
uhkai4212 <kyeburke1@gmail.com>
2025-09-01 10:23:25 +1000

    Add 52cff601ff15294d.lua

---
+++ 52cff601ff15294d.lua
@@ -0,0 +1,8 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ClaimTreasure = ReplicatedStorage.Packages.Knit.Services.HolesService.RF.ClaimTreasure
+
+ClaimTreasure:InvokeServer(
+    12,
+    9
+)
