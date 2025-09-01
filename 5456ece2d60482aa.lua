64a1c58b9ffd8cb5ae050b86ab3cdcb80dc3e4e2
uhkai4212 <kyeburke1@gmail.com>
2025-09-01 10:23:38 +1000

    Add 5456ece2d60482aa.lua

---
+++ 5456ece2d60482aa.lua
@@ -0,0 +1,21 @@
+local EntityModules = game:GetService("ReplicatedStorage").ModulesClient.EntityModules
+local gameData = game.ReplicatedStorage:WaitForChild("GameData")
+local floor = gameData:WaitForChild("Floor")
+local isMines = floor.Value == "Mines"
+local isHotel = floor.Value == "Hotel"
+local isBackdoor = floor.Value == "Backdoor"
+local isGarden = floor.Value == "Garden"
+
+function Distance(pos)
+	if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+		return (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - pos).Magnitude
+	end
+end
+
+_G.GetOldBright = {
+	Brightness = game.Lighting.Brightness,
+	ClockTime = game.Lighting.ClockTime,
+	FogEnd = game.Lighting.FogEnd,
+	GlobalShadows = game.Lighting.GlobalShadows,
+	OutdoorAmbient = game.Lighting.OutdoorAmbient
+}
